54b974071556d8e303c6ab9d7c094e0d2b153630
Manar Mansour <manar.mansour@aucegypt.edu>
2022-12-28 16:50:30 +0200

    fixed an issue in the payments tests

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,5 +1,8 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_KEY!, {
-  apiVersion: '2022-11-15'
-});
+export const stripe = new Stripe(
+  'sk_test_51MIBIXCp9XMqHneaScAze2bDVpYDn1PK4qAZrkTEF3B4YmKJLyEBMu3AfjCOZsawKqnCI7STVkKWX2x6XrdT2Pd000sVQwGMeC',
+  {
+    apiVersion: '2022-11-15'
+  }
+);
